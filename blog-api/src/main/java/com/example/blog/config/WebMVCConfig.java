5cdef8b9f7dd00a1d85307a60be60e0528e3ffe8
vagrant <vagrant.yuxiang@gmail.com>
2022-06-04 11:31:34 +0800

    Add a login interceptor and use ThreadLocal to save user information

--- blog-api/src/main/java/com/example/blog/config/WebMVCConfig.java
+++ blog-api/src/main/java/com/example/blog/config/WebMVCConfig.java
@@ -1,15 +1,27 @@
 package com.example.blog.config;
 
+import com.example.blog.hander.LoginInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMVCConfig implements WebMvcConfigurer {
+    @Autowired
+    private LoginInterceptor loginInterceptor;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         //跨域配置，不可设置为*，不安全, 前后端分离项目，可能域名不一致
         //本地测试 端口不一致 也算跨域
         registry.addMapping("/**").allowedOrigins("http://localhost:8080");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        //假设拦截test接口后继续实际遇到拦截的接口时，再配置真正的接口
+        registry.addInterceptor(loginInterceptor).addPathPatterns("/test");
     }
 }
---
+++ blog-api/src/main/java/com/example/blog/dao/pojo/ArticleBody.java
@@ -0,0 +1,13 @@
+package com.example.blog.dao.pojo;
+
+import lombok.Data;
+//内容表
+@Data
+public class ArticleBody {
+
+    private Long id;
+    private String content;
+    private String contentHtml;
+    private Long articleId;
+}
+
